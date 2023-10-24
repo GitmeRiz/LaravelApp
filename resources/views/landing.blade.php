6b887cfeed92c7997655f764cacbe7a7ed956b0c
GitmeRiz <gitmeriz@gmail.com>
2023-10-24 11:37:37 +0700

    update langding and css

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -4,8 +4,8 @@
 
 @section('content')
     <div class="hero-section animate__animated animate__fadeIn">
-        <h1 class="animate__animated animate__bounce">Welcome!</h1>
-        <p class="animate__animated animate__fadeInLeft">Click Button Below to Explore.</p>
+        <h1 class="animate__animated animate__bounce">Congratulations!</h1>
+        <h2 class="animate__animated animate__fadeInLeft">You have succesfully deploy your Laravel Project with Docker.</h2>
         <a href="{{ route('countdown') }}" class="cta-button animate__animated animate__pulse animate__infinite">Click Me!</a>
     </div>
 
